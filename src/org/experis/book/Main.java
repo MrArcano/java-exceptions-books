7d23f2c2e2447d99a04a664886b3419b548e89ad
MrArcano <salvatore-dario@hotmail.it>
2024-04-03 15:36:33 +0200

    [edit]: exception message

--- src/org/experis/book/Main.java
+++ src/org/experis/book/Main.java
@@ -9,48 +9,57 @@ public class Main {
         int size = 0;
         boolean isNumber = false;
         do {
-            System.out.print("Quanti libri vuoi inserire: ");
+            System.out.print("How many books do you want to enter: ");
             try{
                 size = Integer.parseInt(scan.nextLine());
                 isNumber = true;
             }catch (NumberFormatException e){
+                System.out.println("-------------------------");
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("-------------------------");
+
                 isNumber = false;
             }
-        }while(!isNumber);
-
-
-
+        }while(!isNumber || size < 0);
 
         Book[] books = new Book[size];
-        // Chiede i dati di un libro (titolo, numero pagine, autore, editore), istanzia un nuovo oggetto della classe Book e lo inserisce nell’array. Lo fa per il numero di elementi previsti.
-        for (int i = 0; i < size; i++) {
-            System.out.print("Inserisci titolo: ");
-            String title = scan.nextLine();
 
+        // Chiede i dati di un libro (titolo, numero pagine, autore, editore), istanzia un nuovo oggetto della classe Book e lo inserisce nell’array. Lo fa per il numero di elementi previsti.
+        for (int i = 0; i < size;) {
+            System.out.println("Book #" + (i+1));
+            System.out.print("Insert title: ");
+            String title = scan.nextLine();
 
             int page = 0;
             do {
-                System.out.print("Inserisci numero pagine: ");
+                System.out.print("Insert number of pages: ");
                 try{
                     page = Integer.parseInt(scan.nextLine());
                     isNumber = true;
                 } catch (NumberFormatException e) {
+                    System.out.println("-------------------------");
                     System.out.println("Error: " + e.getMessage());
+                    System.out.println("-------------------------");
+
                     isNumber = false;
                 }
             }while (!isNumber);
 
-            System.out.print("Inserisci autore: ");
+            System.out.print("Insert author: ");
             String author = scan.nextLine();
-            System.out.print("Inserisci editore: ");
+            System.out.print("Insert publisher: ");
             String editor = scan.nextLine();
 
             // Gestire (o anticipare) eventuali errori in modo che il programma non vada in crash.
             try{
                 Book book = new Book(title,page,author,editor);
                 books[i] = book;
+                i++;
             } catch (Exception e) {
-                System.out.println(e.getMessage());
+                System.out.println("-------------------------");
+                System.out.println("Error: " + e.getMessage());
+                System.out.println("-------------------------");
+
             }
         }
 
